e31a57613122f0dc2e40feea1a2f699455a26f45
joseucarvajal <joseucarvajal@gmail.com>
2024-09-05 18:41:54 -0500

    heroku deployment files added

--- app.py
+++ app.py
@@ -26,4 +26,5 @@ def echo():
     return jsonify({'response': param})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
